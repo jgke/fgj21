51dce4e0443327db0981cd7df07bd84774bd41d4
Jaakko Hannikainen <jgke@jgke.fi>
2021-01-30 10:43:40 +0200

    Fix loader

--- front/src/assets.ts
+++ front/src/assets.ts
@@ -2,26 +2,27 @@ import * as PIXI from 'pixi.js';
 
 export interface Assets {
     nopat: PIXI.Sprite
+    tmpmap: PIXI.Sprite
 }
 
 export const assets: Assets = {} as any;
 
 const images: {[k in keyof typeof assets]: any} = {
-    "nopat": require('../assets/img/nopat.png')
+    "nopat": require('../assets/img/nopat.png'),
+    "tmpmap": require('../assets/img/tmpmap.png'),
 };
 
+const loadResource: (next: Promise<string>, key:string) => Promise<string> = (next, key) =>
+    next.then(_ =>
+        new Promise(resolve =>
+            PIXI.Loader.shared.add(key, images[key]).load((loader, resources) => {
+                console.log("Loaded", key);
+                assets[key] = resources[key];
+                resolve(key);
+            })));
+
+
 export function loadAssets(): Promise<Assets> {
-    return Promise.all(
-        Object.keys(images).map(key => {
-            return new Promise(resolve => {
-                PIXI.Loader.shared.add(key, images[key]).load((loader, resources) => {
-                    console.log("Loaded", key);
-                    assets[key] = resources[key];
-                    resolve(key);
-                })
-            })
-        })).then(_res =>{
-            console.log("Assets loaded!");
-            return assets;
-        } );
+    return Object.keys(images).reduce<Promise<string>>(loadResource, new Promise(resolve => resolve("")))
+        .then(_ => assets)
 }
